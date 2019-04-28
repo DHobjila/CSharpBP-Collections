5e3db9f2cc7794a0044b51c212be69d406d89e10
DHobjila <49522840+DHobjila@users.noreply.github.com>
2019-04-28 18:19:44 +0300

    array methods

--- AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -11,6 +11,47 @@ namespace Acme.Biz.Tests
     [TestClass()]
     public class VendorRepositoryTests
     {
+        [TestMethod()]
+        public void RetreiveValueTest()
+        {
 
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = 42;
+
+            //Act
+            var actual = repository.RetreiveValue<int>("Select..", 42);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetreiveValueStringTest()
+        {
+
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = "test";
+
+            //Act
+            var actual = repository.RetreiveValue<string>("Select..", "test");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetreiveValueObjectTest()
+        {
+
+            //Arrange
+            var repository = new VendorRepository();
+            var vendor = new Vendor();
+            var expected = vendor;
+
+            //Act
+            var actual = repository.RetreiveValue<Vendor>("Select..", vendor);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
